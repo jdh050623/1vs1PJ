5d474ad5f3345cf641f9bdbedde9e6272245d4fa
jdh050623 <jdh050623@gmail.com>
2022-05-19 23:17:52 +0900

    test

--- Assets/Script/dd.cs
+++ Assets/Script/dd.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dd : MonoBehaviour
+public class dd: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
