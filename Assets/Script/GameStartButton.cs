1de1e1ddae445c369808530bfb3eff79f074edc4
jdh050623 <jdh050623@gmail.com>
2022-05-19 23:24:19 +0900

    test

--- Assets/Script/GameStartButton.cs
+++ Assets/Script/GameStartButton.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameStartButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void StartButton()//시작 버튼을 클릭시 실행
+    {
+        SceneManager.LoadScene("Stage01");
     }
 }
