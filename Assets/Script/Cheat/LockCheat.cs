c711bbda453e92271db73dccf078b465c7005e7c
jdh050623 <jdh050623@gmail.com>
2022-07-06 00:35:55 +0900

    UI변경,치트,버그수정

---
+++ Assets/Script/Cheat/LockCheat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCheat : MonoBehaviour
+{
+    public void CheatBT()
+    {
+        LockOnOff.instance.GameCle = 24;
+    }
+}
