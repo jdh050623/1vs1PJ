c247c6d9e25de2d8bb67a9338cb51d1dba2ef680
jdh050623 <jdh050623@gmail.com>
2023-04-10 08:46:24 +0900

    .

--- Assets/Script/Cheat/LockCheat.cs
+++ Assets/Script/Cheat/LockCheat.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class LockCheat : MonoBehaviour
 {
+    bool Che = false;
+
+    public static LockCheat instance = null;
+
+    private void Awake()
+    {
+        if (instance == null) //instance가 null. 즉, 시스템상에 존재하고 있지 않을때
+        {
+            instance = this; //내자신을 instance로 넣어줍니다.
+            DontDestroyOnLoad(gameObject); //OnLoad(씬이 로드 되었을때) 자신을 파괴하지 않고 유지
+        }
+        else
+        {
+            if (instance != this) //instance가 내가 아니라면 이미 instance가 하나 존재하고 있다는 의미
+                Destroy(this.gameObject); //둘 이상 존재하면 안되는 객체이니 방금 AWake된 자신을 삭제
+        }
+    }
+
+    public void Update()
+    {
+
+        if (Che == true)
+        {
+            LockOnOff.instance.GameCle = 24;
+        }
+    }
     public void CheatBT()
     {
         LockOnOff.instance.GameCle = 24;
+        Che = true;
+
     }
 }
